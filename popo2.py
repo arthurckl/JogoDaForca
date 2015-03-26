9da302f2cb4ff2b92e269baaf9cd3e3c33aa0bf3
arthurckl <arthurcklee@hotmail.com>
2015-03-26 14:45:01 -0300

    Jogo da Forca 2
    Segundo commit da realização do projeto (23/03)
    Importação do arquivo com os textos para o jogo

---
+++ popo2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 11:53:40 2015
+
+@author: Arthur
+"""
+
+import random
+import turtle
+window = turtle.Screen()
+window.bgcolor("white")
+turtle = turtle.Turtle()
+
+question1 = window.textinput("Jogo da Forca", "Vamos jogar?")
+
+
+
+arquivo = open("entrada.txt", encoding = "utf - 8")
+print (arquivo)
+turtle.title("JOGO DA FORCA!!!")
+
+
+
+
+
+
+
+    
+
